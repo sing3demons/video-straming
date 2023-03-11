629d05e18aba3549c3881dab3bca4c0d8783c833
sing3demons <sing3demons@live.com>
2023-03-11 13:14:05 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+
+app.use("/", express.static("/"));
+
+app.get("/video", (req, res) => {
+  const range = req.headers.range;
+  const videoPath = "./video2548186753.mp4";
+  const videoSize = fs.statSync(videoPath).size;
+
+  const chunkSize = 1 * 1e6;
+  const start = Number(range.replace(/\D/g, ""));
+  const end = Math.min(start + chunkSize, videoSize - 1);
+
+  const contentLength = end - start + 1;
+
+  const headers = {
+    "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+    "Accept-Ranges": "bytes",
+    "Content-Length": contentLength,
+    "Content-Type": "video/mp4",
+  };
+  res.writeHead(206, headers);
+
+  const stream = fs.createReadStream(videoPath, { start, end });
+  stream.pipe(res);
+});
+
+app.listen("3000",()=>console.log("server started"));
